2aa64eaf1c6eefed41b6ac64a9039effc25f08b7
Andy LaBounty <alabounty23@gmail.com>
2021-12-02 10:51:29 -0600

    PSI statistics

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,12 @@ MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg))
+
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- summarize(Suspension_Coil, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =sd(PSI))
+
+group <- group_by(Suspension_Coil, Manufacturing_Lot)
+
+lot_summary <- summarize(group, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD =sd(PSI))
+
